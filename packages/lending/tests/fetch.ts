85114617e8a0417b7bbe5d9a898198ccbb4466d6
yan <yanhaibiao1991@gmail.com>
2025-07-17 15:57:57 +0800

    test: fetch

--- packages/lending/tests/fetch.ts
+++ packages/lending/tests/fetch.ts
@@ -9,6 +9,7 @@
       ...init,
       headers: {
         ...init?.headers,
+        Host: 'app.naviprotocol.io',
         'User-Agent':
           'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'
       },
