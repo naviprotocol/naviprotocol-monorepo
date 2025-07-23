95b6f0ea4b4136473e47893aa1ce71a79a578120
yan <yanhaibiao1991@gmail.com>
2025-07-23 12:27:11 +0800

    docs

--- packages/docs/app/[[...slug]]/layout.tsx
+++ packages/docs/app/[[...slug]]/layout.tsx
@@ -38,6 +38,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             title: 'NAVI SDK Migration',
             description: 'migration from navi-sdk package',
             url: '/navi-sdk-migration/lending',
+            urls: new Set([
+              '/navi-sdk-migration/lending',
+              '/navi-sdk-migration/account-manager',
+              '/navi-sdk-migration/swap',
+              '/navi-sdk-migration/bridge'
+            ]),
             icon: <Image src="/assets/logo.png" alt="Lending" width={24} height={24} />
           }
         ]
