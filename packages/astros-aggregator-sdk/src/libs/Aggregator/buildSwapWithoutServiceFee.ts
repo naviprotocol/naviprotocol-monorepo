a5b88bb21de07459f30b343c2174fdb8ffd13174
yan <yanhaibiao1991@gmail.com>
2025-08-27 17:03:29 +0800

    fix decimal

--- packages/astros-aggregator-sdk/src/libs/Aggregator/buildSwapWithoutServiceFee.ts
+++ packages/astros-aggregator-sdk/src/libs/Aggregator/buildSwapWithoutServiceFee.ts
@@ -311,7 +311,7 @@ export async function buildSwapWithoutServiceFee(
       txb.pure.u64(Math.floor(minAmountOut)), // negative slippage
       txb.pure.u64(quote.amount_out), // expected amount out, the dry run swap output coin amount
       txb.pure.u64(quote.amount_in), // amount in
-      txb.pure.u64(amountInValue), // amount in value
+      txb.pure.u64(Math.floor(amountInValue)), // amount in value
       txb.pure.u64(referral) // referral
     ],
     typeArguments: [tokenA, tokenB]
